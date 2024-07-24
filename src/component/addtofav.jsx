6e5990446ab3336bca823cfde850086ab525dd37
Ajay <mailtoajay7007@gmail.com>
2024-07-24 17:04:19 +0530

    stylings

--- src/component/addtofav.jsx
+++ src/component/addtofav.jsx
@@ -22,25 +22,26 @@ export default function Addtofav() {
 
   return (
     <div>
-      <div className="cate grid grid-cols-3 gap-3 m-4">
+    <h1 className="text-6xl font-bold text-center">Add To Favorites</h1>
+      <div className="cate grid lg:grid-cols-3 sm:grid-cols-1 gap-3 m-4 ">
         {data &&
           data.categories.map((item) => (
-            <div key={item.idCategory} className="flex flex-col items-center justify-center bg-gray-300">
-              <img src={item.strCategoryThumb} alt="" />
-              <h3>{item.strCategory}</h3>
+            <div key={item.idCategory} className="flex flex-col items-center justify-center bg-gray-300 p-4 rounded-lg hover:cursor-pointer">
+              <img src={item.strCategoryThumb} alt=""  className=" rounded-md hover:scale-110 transition-transform "/>
+              <h3 className="text-2xl font-serif hover:italic">{item.strCategory}</h3>
               {fav.some(f => f.idCategory === item.idCategory) ? (
                 <button
                   onClick={() =>
                     setFav(fav.filter((f) => f.idCategory !== item.idCategory))
                   }
-                  className="btn h-14 w-40 rounded-md hover:bg-red-400 bg-red-700"
+                  className="btn h-12 w-36 rounded-md hover:bg-red-600 bg-red-700  m-2 hover:text-white hover:scale-110 hover:font-medium transition-all"
                 >
                   Remove from Fav
                 </button>
               ) : (
                 <button
                   onClick={() => setFav([...fav, item])}
-                  className="btn h-14 w-40 rounded-md hover:bg-green-400 bg-green-700"
+                  className="btn h-12 w-36 rounded-md hover:bg-green-400 bg-green-700 m-2 hover:text-white hover:scale-110 hover:font-medium transition-all"
                 >
                   Add to Fav
                 </button>
@@ -52,25 +53,25 @@ export default function Addtofav() {
       <h1 className="text-center text-6xl font-bold">Favorites</h1>
       
 
-      <div className="favs parent grid grid-cols-3 gap-3">
+      <div className="favs parent grid lg:grid-cols-3 sm:grid-cols-1 gap-3 m-4">
         {fav && fav.length ? (
           fav.map((item) => (
-            <div key={item.idCategory} className="flex flex-col items-center justify-center">
-              <h3>{item.strCategory}</h3>
+            <div key={item.idCategory} className="flex flex-col items-center justify-center bg-gray-300  rounded-xl ">
+              <h3 className="text-2xl font-serif hover:italic">{item.strCategory}</h3>
               <img src={item.strCategoryThumb} alt="" />
               
               <button
                 onClick={() =>
                   setFav(fav.filter((f) => f.idCategory!== item.idCategory))
                 }
-                className="btn h-14 w-40 rounded-md hover:bg-red-400 bg-red-700"
+                className="btn h-12 w-36 rounded-md hover:bg-red-600 bg-red-700  hover:text-white hover:scale-110 hover:font-medium transition-all m-2"
               >
                 Remove from Fav
               </button>
             </div>
           ))
         ) : (
-          <span>Nothing To show</span>
+          <h4 className="text-3xl font-medium italic text-center w-full">Nothing To show !</h4>
         )}
       
       </div>
